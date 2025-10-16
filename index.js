802fe2f72ab2eb65fba7a1e986e447c7d4bce696
Abrar <araha002@gold.ac.uk>
2025-10-16 14:07:28 +0100

    New Paragraph

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8000;
 http.createServer(function(req, res) { 
     //console.log(req);
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>My First Webpage</title></head><body><h1>Hello World!</h1><h2>Ciao Mondo!</h2><p>こんにちは世界!</p></body></html>");
+    res.end("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>My First Webpage</title></head><body><h1>Hello World!</h1><h2>Ciao Mondo!</h2><p>こんにちは世界!</p><p>NEW PARAGRAPH</p></body></html>");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
